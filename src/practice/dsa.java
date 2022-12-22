4101c8c9bc66ea1d33b4ce098521786939122fb3
jayesh315githubber <114106014+jayesh315githubber@users.noreply.github.com>
2022-12-22 17:52:41 +0530

    Update dsa.java

--- src/practice/dsa.java
+++ src/practice/dsa.java
@@ -4,6 +4,8 @@ public class dsa {
 	public static void main(String[] args) {
 		System.out.println("hello world.");
 		System.out.println("hello world 2.");
+		System.out.println("welcome");
+		System.out.println("pune");
 	}
 
 }
