1b01e98514a42f2056aada5b40edf9d607cfc7ba
jayesh315githubber <jayeshgangurde315@gmail.com>
2022-12-22 17:54:21 +0530

    second commit

--- src/practice/dsa.java
+++ src/practice/dsa.java
@@ -4,6 +4,7 @@ public class dsa {
 	public static void main(String[] args) {
 		System.out.println("hello world.");
 		System.out.println("hello world 2.");
+		System.out.println("1234567");
 	}
 
 }
